f9c02238ec1cfd6509a37add123843e39e265a2f
Rick Poleshuck <RickPoleshuck@gmail.com>
2023-01-18 13:05:33 -0600

    spanish testing

--- test/num2words_test.dart
+++ test/num2words_test.dart
@@ -1,13 +1,26 @@
+import 'dart:math';
+
 import 'package:num2words/num2words.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('Tests', ()
   {
-    final num2words = Num2Words(language: 'en_US');
+    var num2words = Num2Words(language: 'en_US');
     test('whole numbers', () {
       for (int i = 0; i < 10000; ++i) {
         print(num2words.formatWhole(i));
+      }
+      // expect(num2words.format(123), 'value');
+    });
+
+
+    test('Mexican money', () {
+      num2words = Num2Words(language: 'es_MX');
+      for (double i = 0; i < 10000; ++i) {
+        final dec = Random().nextDouble();
+        final value = double.parse((i + dec).toStringAsFixed(2));
+        print('${value.toStringAsFixed(2)}: ${num2words.formatMoney(value)}');
       }
       // expect(num2words.format(123), 'value');
     });
